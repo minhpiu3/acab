7125e144ab76f6b20628f3ccbf55951d0ab861ac
minhpiu3 <chamchikiemtien0003@gmail.com>
2025-01-10 15:35:43 +0700

    Update save_data.php

--- save_data.php
+++ save_data.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     $data = "Họ và tên: $name\nSố điện thoại: $phone\nNội dung: $message\n\n";
 
-    $file = 'form_data.txt'; // Tên tệp để lưu dữ liệu
+    $file = 'data/form_data.txt'; // Đường dẫn tệp trong thư mục 'data'
     file_put_contents($file, $data, FILE_APPEND);
 
     echo "Cảm ơn bạn đã liên hệ! Dữ liệu của bạn đã được lưu.";
